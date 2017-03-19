799b40f3bdd62d65e9b5378d47003a9acc37dc96
glavak <gau-gau@ya.ru>
2017-03-19 12:19:50 +0700

    Eval works
    With some weird method

--- Eval/EvalProgram.cs
+++ Eval/EvalProgram.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Globalization;
 
 namespace EvalTask
 {
@@ -7,8 +9,20 @@ namespace EvalTask
 		static void Main(string[] args)
 		{
 			string input = Console.In.ReadToEnd();
-			string output = "TODO";
+            
+			string output = Evaluate(input).ToString(CultureInfo.InvariantCulture);
+
 			Console.WriteLine(output);
-		}
-	}
+        }
+
+
+        public static double Evaluate(string expression)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("expression", typeof(string), expression);
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+            return double.Parse((string)row["expression"]);
+        }
+    }
 }
