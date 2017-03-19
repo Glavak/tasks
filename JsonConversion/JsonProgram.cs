b2c06f6bef363d92c73ba705bde865a4978fb816
Kirill <xauhekeh@gmail.com>
2017-03-19 15:20:01 +0500

    Long lines

--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -87,7 +87,9 @@ namespace JsonConversion
                         "\"2\": {\"name\": \"Pencil\",\"price\": 8,\"count\": 1000}," +
                         "\"3\": {\"name\": \"Box\",\"price\": 12.1,\"count\": 50}}}";
             var v2Json = JObject.Parse(v2String).ToObject<JsonV2Model>();
-            string v3String = "{\"version\":\"3\",\"products\":[{\"id\":1,\"name\":\"Pen\",\"price\":12,\"count\":100},{\"id\":2,\"name\":\"Pencil\",\"price\":8,\"count\":1000},{\"id\":3,\"name\":\"Box\",\"price\":12.1,\"count\":50}]}";
+            string v3String = "{\"version\":\"3\",\"products\":[{\"id\":1,\"name\":\"Pen\",\"price\":12,\"count\":100}," +
+                "{\"id\":2,\"name\":\"Pencil\",\"price\":8,\"count\":1000}," +
+                "{\"id\":3,\"name\":\"Box\",\"price\":12.1,\"count\":50}]}";
             var result = JsonConvert.SerializeObject(ConvertV2ToV3.ConvertToV3(v2Json));
 
             Assert.AreEqual(v3String, result);
@@ -105,9 +107,20 @@ namespace JsonConversion
         }
 
         [Test]
-        public void DoSomething_WhenSomething()
+        public void TestFormulaInPriceAnotherCase()
         {
-            string v2String = "{\"version\":\"2\",\"constants\":{\"p\":375570429.0,\"BDT5b\":1895861230.0,\"pk6rq0teL\":2147483647.0,\"d801p5J6\":1994942430.0,\"GuuguI\":0.0},\"products\":{\"0\":{\"name\":\"RQbl0WfVY\",\"price\":\"(-84.06633771214)*(p)\",\"count\":881891749},\"1\":{\"name\":\"kDaf0Z\",\"price\":\"(12.1639563293028)+(p)\",\"count\":2147483647},\"304322859\":{\"name\":\"KcOi9dvy\",\"price\":\"(pk6rq0teL)/(-37.0159578216336)\",\"count\":1},\"1408114756\":{\"name\":\"v4N\",\"price\":\"(-85.7739047081088)/(37.9361148634628)\",\"count\":1},\"1889271802\":{\"name\":\"wtXpL\",\"price\":\"(13.8310322136763)*(58.0251485845191)\",\"count\":0},\"687275581\":{\"name\":\"bp\",\"price\":\"(-92.3887056263111)-(d801p5J6)\",\"count\":0},\"1732466776\":{\"name\":\"IohrwvzQ\",\"price\":\"(d801p5J6)+(41.3040054688715)\",\"count\":2147483647},\"2147483647\":{\"name\":\"Abap\",\"price\":\"(-59.916888624391)-(-66.5829328664499)\",\"count\":2147483647},\"517841970\":{\"name\":\"KD9S4\",\"price\":\"(58.4912672445603)/(p)\",\"count\":2147483647},\"1362982700\":{\"name\":\"9pH\",\"price\":\"(-54.4264007147524)/(-45.6487527795363)\",\"count\":1}}}";
+            string v2String = "{\"version\":\"2\"," +
+                "\"constants\":{\"p\":375570429.0,\"BDT5b\":1895861230.0,\"pk6rq0teL\":2147483647.0,\"d801p5J6\":1994942430.0,\"GuuguI\":0.0}," +
+                "\"products\":{\"0\":{\"name\":\"RQbl0WfVY\",\"price\":\"(-84.06633771214)*(p)\",\"count\":881891749}," +
+                "\"1\":{\"name\":\"kDaf0Z\",\"price\":\"(12.1639563293028)+(p)\",\"count\":2147483647}," +
+                "\"304322859\":{\"name\":\"KcOi9dvy\",\"price\":\"(pk6rq0teL)/(-37.0159578216336)\",\"count\":1}," +
+                "\"1408114756\":{\"name\":\"v4N\",\"price\":\"(-85.7739047081088)/(37.9361148634628)\",\"count\":1}," +
+                "\"1889271802\":{\"name\":\"wtXpL\",\"price\":\"(13.8310322136763)*(58.0251485845191)\",\"count\":0}," +
+                "\"687275581\":{\"name\":\"bp\",\"price\":\"(-92.3887056263111)-(d801p5J6)\",\"count\":0}," +
+                "\"1732466776\":{\"name\":\"IohrwvzQ\",\"price\":\"(d801p5J6)+(41.3040054688715)\",\"count\":2147483647}," +
+                "\"2147483647\":{\"name\":\"Abap\",\"price\":\"(-59.916888624391)-(-66.5829328664499)\",\"count\":2147483647}," +
+                "\"517841970\":{\"name\":\"KD9S4\",\"price\":\"(58.4912672445603)/(p)\",\"count\":2147483647}," +
+                "\"1362982700\":{\"name\":\"9pH\",\"price\":\"(-54.4264007147524)/(-45.6487527795363)\",\"count\":1}}}";
             var v2Test = JObject.Parse(v2String).ToObject<JsonV2Model>();
             var result = JsonConvert.SerializeObject(ConvertV2ToV3.ConvertToV3(v2Test));
 
