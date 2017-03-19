549ae56faf3592a9ae970e59fc1f76ad6ba5b147
Kirill <xauhekeh@gmail.com>
2017-03-19 11:47:56 +0500

    task 1

--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using Newtonsoft.Json;
+using NUnit.Framework;
 
 namespace JsonConversion
 {
@@ -8,10 +10,44 @@ namespace JsonConversion
 		static void Main()
 		{
 			string json = Console.In.ReadToEnd();
-			JObject v2 = JObject.Parse(json);
-			//...
-			var v3 = "{ 'version':'3', 'products': 'TODO' }";
-			Console.Write(v3);
+            var v2 = JObject.Parse(json).ToObject<JsonV2Model>();
+
+            Console.WriteLine(JsonConvert.SerializeObject(ConvertV2ToV3.ConvertToV3(v2)));
 		}
 	}
+
+    public static class ConvertV2ToV3
+    {
+        public static JsonV3Model ConvertToV3(JsonV2Model v2Model)
+        {
+            var v3Model = new JsonV3Model();
+            foreach (var model in v2Model.Products)
+            {
+                v3Model.products.Add(new ProductV3Model()
+                {
+                    id = model.Key,
+                    count = model.Value.count,
+                    name = model.Value.name,
+                    price = model.Value.price
+                });
+            }
+            return v3Model;
+        }
+    }
+
+    [TestFixture]
+    public class JsonProgram_Should
+    {
+        [Test]
+        public void test_something()
+        {
+            string v2 = "{\"version\": \"2\",\"products\": {\"1\": {\"name\": \"Pen\",\"price\": 12,\"count\": 100}," +
+                        "\"2\": {\"name\": \"Pencil\",\"price\": 8,\"count\": 1000}," +
+                        "\"3\": {\"name\": \"Box\",\"price\": 12.1,\"count\": 50}}}";
+
+            var s = JObject.Parse(v2).ToObject<JsonV2Model>();
+            //Console.Out.WriteLine("s = {0}", s);
+
+        }
+    }
 }
---
+++ JsonConversion/JsonV2Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConversion
+{
+    public class JsonV2Model
+    {
+        public JsonV2Model()
+        {
+            Products = new Dictionary<int, ProductV2Model>();
+            version = "2";
+        }
+
+        public string version;
+        public Dictionary<int, ProductV2Model> Products;
+    }
+
+    public class ProductV2Model
+    {
+        public string name;
+        public int price;
+        public int count;
+    }
+}
